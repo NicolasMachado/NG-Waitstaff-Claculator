c00a4ec07bd361986557b70640c90ff474f2f2b2
NicolasMachado <nicoma63@gmail.com>
2017-06-09 12:43:04 +0200

    assignement done

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 angular.module('myApp', [])
-  .controller('MultiplicationCtrl', function($attrs) {
+  .controller('WaitStaffCtrl', function($scope) {
     var vm = this;
+
+    $scope.submit = function() {
+      if (!vm.mdBaseMealPrice) {
+        return
+      }
+      vm.ccSubTotal = 0;
+      vm.ccTip = 0;
+      console.log(vm.mdBaseMealPrice);
+      vm.ccSubTotal = vm.mdBaseMealPrice + (vm.mdBaseMealPrice*vm.mdTaxRate/100);
+      vm.ccTip = vm.mdBaseMealPrice*vm.mdTipPercentage/100;
+
+      vm.eiTipTotal += vm.ccTip;
+      vm.eiMealCount ++;
+      vm.mdBaseMealPrice = null;
+    }
+
+    $scope.init = function() {
+      vm.ccSubTotal = 0;
+      vm.ccTip = 0;
+
+      vm.eiTipTotal = 0;
+      vm.eiMealCount = 0;
+
+      vm.mdBaseMealPrice = null;
+      vm.mdTaxRate = 20;
+      vm.mdTipPercentage = 15;
+    }
+    $scope.init();
   });
